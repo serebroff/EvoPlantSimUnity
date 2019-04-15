2e879525ecd6bd182a1f7f48795c6555d791e3a5
serebroff <serebroff@gmail.com>
2019-04-15 14:34:02 +0300

    First commit

---
+++ Assets/SunBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBehaviour : MonoBehaviour
+{
+    public Transform  tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive!");
+        tr = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x += 0.1f;
+        transform.position = pos;
+                
+
+    }
+}
