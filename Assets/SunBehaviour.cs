fc4c65261f40fddb682eb7c7cbaec14e9d515dec
serebroff <serebroff@gmail.com>
2019-04-16 18:28:02 +0300

    Sun rotates now

--- Assets/SunBehaviour.cs
+++ Assets/SunBehaviour.cs
@@ -1,25 +1,71 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SunBehaviour : MonoBehaviour
 {
-    public Transform  tr;
+    Vector2 pos;
+    Vector2 center;
+    Vector2 dir;
+
+    public float radius;
+    float time = 0;
+
+   // Beam[] ar_beams;
+
+
+    public float scaledPixelHeight;
+    public float screenheight;
+    public int num_beams;
+
+    GameObject sun;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I am alive!");
-        tr = GetComponent<Transform>();
-        
+        radius = 10;
+        center.Set(0, -radius / 2);
+        pos.Set(0, 0);
+        time = 0;
+
+        num_beams = 100;
+      //  ar_beams = new Beam[num_beams];
+
+        sun = GameObject.Find("sun");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
-        pos.x += 0.1f;
-        transform.position = pos;
-                
+        time += Time.deltaTime * 0.1f;
+
+        dir.Set(Mathf.Cos(time), Mathf.Sin(time));
+
+        pos = center + radius * dir;
+
+
+
+        sun.transform.position = pos;
+        sun.transform.Rotate(new Vector3(0, 0, Time.deltaTime * 10f));
+
+        int i = 0;
+    /*    foreach (Beam b in ar_beams)
+        {
+            i++;
+            b.pos = pos + i * Vector2.Perpendicular(dir);
+        }*/
+
+
+        ///////////
+
+        
+        screenheight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).x;
 
     }
+
+
+
 }
